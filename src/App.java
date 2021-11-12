204862933f5228213f54545c93d3fecac72c2b76
shakiralynch1 <92158341+shakiralynch1@users.noreply.github.com>
2021-11-12 16:49:54 +0000

    Creating classes in car project

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Tesla Co. System" );
+        App app = new App();
+        app.start();
+    }
+
+    public void start() {
+        
+
+    }
+}
